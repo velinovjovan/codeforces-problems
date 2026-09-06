ff8701838b0ec21c1996ae8d3a15eb6b7ad53d85
Jovan <jowwanv@gmail.com>
2026-09-06 15:14:02 +0200

    added problems

--- problems/5C.cpp
+++ problems/5C.cpp
@@ -1,54 +1,45 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int main(){
-	ios_base::sync_with_stdio(false);
-	cin.tie(nullptr);
-	
-	string s;
-	cin >> s;
-	
-	stack<char> stek;
-	
-	int curr = 0;
-	int maks = 0;
-	int ans = 0;
-	
-	for(auto &c : s){
-		if(stek.empty()){
-			if(c == '('){
-				stek.push(c);
-			}
-			else{
-				if(curr){
-					maks = max(maks, curr);
-					ans ++;
-					curr = 0;
-				}
-			}
-		}
-		else{
-			if(c == '('){
-				stek.push(c);
-			}
-			else{
-				stek.pop();
-				curr++;
-			}
-		}
-	}
-	
-	if(curr){
-		maks = max(maks, curr);
-		ans ++;
-		curr = 0;
-	}
-	
-	
-	if(maks == 0) cout << "0 1" << "\n";
-	else cout << maks * 2 << ' ' << ans << "\n";
-	
-	
-	return 0;
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    string s;
+    cin >> s;
+    
+    stack<int> stek;
+    stek.push(-1);
+    
+    int max_len = 0;
+    int count = 1;
+    
+    for(int i = 0; i < s.length(); i++){
+        if(s[i] == '('){
+            stek.push(i);
+        } 
+		else {
+            stek.pop();
+            
+            if(stek.empty()){
+                stek.push(i);
+            } 
+			else {
+                int current_len = i - stek.top();
+                
+                if(current_len > max_len){
+                    max_len = current_len;
+                    count = 1;
+                } 
+				else if(current_len == max_len){
+                    count++;
+                }
+            }
+        }
+    }
+    
+    cout << max_len << " " << count << "\n";
+    
+    return 0;
 }
